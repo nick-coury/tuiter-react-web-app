a6572fd069d7766af98d9a51da831143ba7c3439
Nick <59811334+nick-coury@users.noreply.github.com>
2022-11-20 21:10:15 -0500

    Add files via upload

---
+++ src/tuiter/App.js
@@ -0,0 +1,26 @@
+import Labs from "./labs";
+import './App.css';
+import HelloWorld from "./labs/a6/hello-world";
+import Tuiter from "./tuiter";
+import {BrowserRouter} from "react-router-dom";
+import {Routes, Route} from "react-router";
+
+function App() {
+  return (
+      <BrowserRouter>
+          <div className="container">
+              <Routes>
+                  <Route path="/*"
+                         element={<Labs/>}/>
+                  <Route path="/hello"
+                         element={<HelloWorld/>}/>
+                  <Route path="/tuiter"
+                         element={<Tuiter/>}/>
+              </Routes>
+
+          </div>
+      </BrowserRouter>
+  );
+}
+export default App;
+
