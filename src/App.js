6e66c8fe44823ace87fca0f6117d398bb6b1228d
Nick <59811334+nick-coury@users.noreply.github.com>
2022-11-30 23:08:36 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import HelloWorld from "./labs/a6/hello-world";
 import Tuiter from "./tuiter";
 import {BrowserRouter} from "react-router-dom";
 import {Routes, Route} from "react-router";
+import mongoose from "mongoose";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb+srv://nick:<password>@cluster0.opkc5eh.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.connect(CONNECTION_STRING);
 
 function App() {
   return (
